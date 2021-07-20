0a2d60fca796aca9361d7ff1712c1d6e3d4e1d5d
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-07-20 20:03:39 +0530

    Initial commit

---
+++ src/Anagrams.java
@@ -0,0 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Anagrams {
+    static boolean isAnagrams(String a,String b){
+        a=a.toUpperCase();
+        b=b.toUpperCase();
+        boolean r=false;
+        StringBuilder c=new StringBuilder(b);
+        if (a.length()==b.length()){
+            for (int i = 0; i < a.length(); i++) {
+                for (int j = 0; j < c.length(); j++) {
+                    if (a.charAt(i)==c.charAt(j)){
+                        c.deleteCharAt(j);
+                        if (i==a.length()-1&&c.length()==0){
+                            r=true;
+                            break;
+                        }
+                    break;
+                    }
+
+                    
+                }
+
+            }
+        }return r;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        String b=sc.next();
+        sc.close();
+        boolean r=isAnagrams(a,b);
+        System.out.println((r) ?    "Anagrams":"Not Anagrams");
+    }
+}
